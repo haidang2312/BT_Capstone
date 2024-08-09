7d0bea958646561250ace41f56c8584502788be3
“UtXuan” <1642017@gmail.com>
2024-08-09 19:35:45 +0700

    commit lần 2
    hoàn thành phần còn lai của committ 1

--- script.js
+++ script.js
@@ -38,5 +38,21 @@ function toggleTextColor(element) {
 }
 
 
+// back to top
+let mybutton = document.getElementById("topButton");
 
-  
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+mybutton.addEventListener("click", topFunction);
